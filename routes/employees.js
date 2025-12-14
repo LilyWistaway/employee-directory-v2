6280ed4bfe11559cc0f62156b57eb24f4c2e7836
Lily <info@wistaway.com>
2025-12-14 14:04:30 -0600

    employees v2

---
+++ routes/employees.js
@@ -0,0 +1,44 @@
+import express from "express";
+import employees from "#db/employees";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send(employees);
+});
+
+router.get("/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  res.send(employees[randomIndex]);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const employee = employees.find((e) => e.id === +id);
+
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employee);
+});
+
+router.post("/", (req, res) => {
+  if (!req.body || !req.body.name) {
+    return res.status(400).send("Name is required");
+  }
+
+  const name = req.body.name.trim();
+  if (name.length === 0) {
+    return res.status(400).send("Name is required");
+  }
+
+  const lastId = employees.length ? employees[employees.length - 1].id : 0;
+  const newEmployee = { id: lastId + 1, name };
+
+  employees.push(newEmployee);
+
+  res.status(201).send(newEmployee);
+});
+
+export default router;
